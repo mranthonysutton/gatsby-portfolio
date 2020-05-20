91d24d739147d2eb7c36cc7557d379657e142047
Anthony Sutton <anthony.sutton95@gmail.com>
2020-05-19 20:11:50 -0700

    Added .eslintrc for support and loaded in some plugins to allow for eslinting. :beers:
    Signed-off-by: Anthony Sutton <anthony.sutton95@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: "babel-eslint",
+  rules: {
+    strict: 0,
+  },
+  plugins: ["react-hooks", "eslint:recommend", "plugin:react/recommend"],
+  extends: ["airbnb"],
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,13 @@ module.exports = {
     linkedin: "https://www.linkedin.com/in/mranthonysutton/",
     github: "https://github.com/mranthonysutton",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        exclude: /(node_modules|.cache|public)/,
+      },
+    },
+  ],
 };
