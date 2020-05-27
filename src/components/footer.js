3648313eaf5fb2d3c6831e3f7d978b7de21c949a
Anthony Sutton <anthony.sutton95@gmail.com>
2020-05-27 14:08:20 -0700

    feat: Adjusted some basic styles for mobile design. Also fixed contact links to open in new tab
    Signed-off-by: Anthony Sutton <anthony.sutton95@gmail.com>

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,18 +10,24 @@ const Footer = () => {
       <section className={footerStyles.iconContainer}>
         <a
           href="https://github.com/mranthonysutton"
+          target="_blank"
+          rel="noopener noreferrer"
           className={footerStyles.iconLink}
         >
           <FaGithub size="1.4rem" />
         </a>
         <a
           href="https://www.linkedin.com/in/mranthonysutton/"
+          target="_blank"
+          rel="noopener noreferrer"
           className={footerStyles.iconLink}
         >
           <FaLinkedin size="1.4rem " />
         </a>
         <a
           href="mailto:anthony.sutton95@gmail.com"
+          target="+blank"
+          rel="noopener noreferrer"
           className={footerStyles.iconLink}
         >
           <FaEnvelope size="1.4rem " />
